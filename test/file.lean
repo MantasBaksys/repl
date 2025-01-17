f266facdf8e2deea56307b85aba8bb7f153ce984
MantasBaksys <baksys.mantas@gmail.com>
2025-01-17 15:28:04 +0000

    add pattern-matching proof

--- test/file.lean
+++ test/file.lean
@@ -5,7 +5,12 @@ def g := 2
 
 theorem i : f + g = 39 := rfl
 
+theorem j : ∀ n m : Nat, n + m = m + n
+| 0, m => Nat.add_comm 0 m
+| .succ n, m => by rw [Nat.add_comm]
+
 theorem k : g + f = 39 := by
   rw [f, g]
+
 
 -- echo '{"path": "test/file.lean", "allTactics": true}' | lake exe repl
